6ea0967e3ec588223208f06f33c82b71922a98c0
Rahul <rahulchodvadiya@gmail.com>
2023-09-02 01:59:40 +0530

    update server host

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import mainRoutes from './routes/index';
 import customerRoutes from './routes/customer';
 
 const app = express();
+const host = process.env.ENV === 'production' ? '0.0.0.0' : 'localhost';
 const port = 3000;
 
 /**
@@ -22,7 +23,7 @@ async function main() {
 		app.use('/', mainRoutes);
 		app.use('/', customerRoutes);
 
-		app.listen(port, () => {
+		app.listen(port, host,  () => {
 			console.log(`Customer service listening on port ${port}`);
 		});
 	} catch (error) {
